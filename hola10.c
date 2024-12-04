fafed5316ef2c3f9a2d74ce2eca7b05519f3e0a9
margar2404 <mgarridocolom@gmail.com>
2024-12-04 11:38:55 +0100

    hola10.c

--- hola10.c
+++ hola10.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(){
-    int i; j;
-    for(i=1;i<=10;i++)
-        for(j=1;j<=10;j++)
-    printf("W");
-    return 0;
+  int i;
+  for(i=1;i<=10;i++)
+    printf("Hola món\n");
+  return 0;
 }
